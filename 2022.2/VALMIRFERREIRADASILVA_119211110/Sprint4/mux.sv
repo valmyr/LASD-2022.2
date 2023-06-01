f17bf2fa432fcd52c0c880ef71d4617842c23337
Valmir Ferreira da Silva <valmir.silva@ee.ufcg.edu.br>
2023-06-01 13:44:14 -0300

    up

--- 2022.2/VALMIRFERREIRADASILVA_119211110/Sprint4/mux.sv
+++
@@ -1,7 +0,0 @@
-module mux2_1(
-    input  logic     sel,
-    input  logic     [7:0] inA, inB,
-    output logic     [7:0] out
-);
-    assign out = sel ? inA : inB;
-endmodule 
--- 2022.2/VALMIRFERREIRADASILVA_119211110/Sprint5/PC.sv
+++
@@ -1,8 +0,0 @@
-module PC#(parameter WIDTH = 8)(
-    input  logic             clk         ,
-                             rst         ,
-    input  logic [WIDTH-1:0] PCin        ,
-    output logic [WIDTH-1:0] PCout         
-);
-    always_ff@(posedge clk or negedge rst) PCout <= (rst == 0)? 0 : PCin;
-endmodule
